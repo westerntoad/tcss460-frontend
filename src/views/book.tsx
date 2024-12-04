2f5a4736ebf6b1d239635f919bd0024dd53105bf
westerntoad <abr.engebretson@gmail.com>
2024-12-04 12:02:26 -0800

    alter book form

--- src/views/book.tsx
+++ src/views/book.tsx
@@ -18,23 +18,30 @@ interface Props {
 export default function BookPage({ book }: Props) {
   return (
     <MainCard>
-      <Stack direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem sx={{ marginBottom: 10 }} />}>
-        <Stack direction="column" spacing={2}>
-          <Image src={book.icons.large} width={98} height={147} alt="Book cover" />
-        </Stack>
-        <Stack direction="column" spacing={2}>
-          <Typography variant="h2">{book.title}</Typography>
-          <Typography variant="body1">{'by ' + book.authors}</Typography>
-          <Typography variant="body1">{'first published ' + book.publication}</Typography>
-          <BookRatings ratings={book.ratings} />
-        </Stack>
-      </Stack>
+      {(book.isbn13 && (
+        <>
+          <Stack direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem sx={{ marginBottom: 10 }} />}>
+            <Stack direction="column" spacing={2}>
+              <Image src={book.icons.large} width={98} height={147} alt="Book cover" />
+            </Stack>
+            <Stack direction="column" spacing={2}>
+              <Typography variant="h2">{book.title}</Typography>
+              <Typography variant="body1">{'by ' + book.authors}</Typography>
+              <Typography variant="body1">{'first published ' + book.publication}</Typography>
+              <BookRatings ratings={book.ratings} />
+            </Stack>
+          </Stack>
 
-      <Typography variant="h3" sx={{ paddingTop: 3 }}>More by These Authors</Typography>
-      <Divider sx={{ paddingTop: 0.5 }} />
-      {book.authors.split(',').map((author: string) => (
-        <WorkBy key={author} author={author} />
-      ))}
+          <Typography variant="h3" sx={{ paddingTop: 3 }}>
+            More by These Authors
+          </Typography>
+          <Divider sx={{ paddingTop: 0.5 }} />
+          {book.authors.split(',').map((author: string) => (
+            <WorkBy key={author} author={author} />
+          ))}
+        </>
+      )) ||
+        'Loading...'}
     </MainCard>
   );
 }
