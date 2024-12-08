085a2bfe9772226c669088e5e902f117199b493e
westerntoad <abr.engebretson@gmail.com>
2024-12-08 15:51:08 -0800

    default book cover changes

--- src/components/BookIcon.tsx
+++ src/components/BookIcon.tsx
@@ -12,10 +12,13 @@ interface Props {
 }
 
 export default function BookIcon({ book }: Props) {
+  const [hasError, setHasError] = React.useState<boolean>(false);
+  const fallbackImage = 'https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png';
+
   return (
     <Tooltip title={<Typography variant="body2">{book.title}</Typography>}>
       <Link href={book.isbn13}>
-        <Image src={book.image_url} width={98} height={147} alt="Book cover" />
+        <Image onError={() => setHasError(true)} src={hasError ? fallbackImage : book.image_url} width={98} height={147} alt="Book cover" />
       </Link>
     </Tooltip>
   );
--- src/views/book.tsx
+++ src/views/book.tsx
@@ -17,13 +17,25 @@ interface Props {
 }
 
 export default function BookPage({ book }: Props) {
+  const [hasError, setHasError] = React.useState<boolean>(false);
+
+  const fallbackImage = 'https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png';
+  const isbnString = '' + book.isbn13;
+  const formatedISBN = `${isbnString.substring(0, 3)}-${isbnString.charAt(3)}-${isbnString.substring(4, 6)}-${isbnString.substring(6, 12)}-${isbnString.charAt(12)}`;
+
   return (
     <MainCard>
       {(book.isbn13 && (
         <>
           <Stack direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem sx={{ marginBottom: 10 }} />}>
             <Stack direction="column" spacing={2}>
-              <Image src={book.icons.large} width={98} height={147} alt="Book cover" />
+              <Image
+                src={hasError ? fallbackImage : book.icons.large}
+                width={98}
+                height={147}
+                alt="Book cover"
+                onError={()=> setHasError(true)} 
+              />
               <Button href={`change/${book.isbn13}`} sx={{ fontSize: 13 }} variant="contained">
                 Alter Book
               </Button>
@@ -34,6 +46,7 @@ export default function BookPage({ book }: Props) {
                 <em>{'by ' + book.authors}</em>
               </Typography>
               <Typography variant="body1">{'First published ' + book.publication}</Typography>
+              <Typography variant="body1">{`ISBN-13: ${formatedISBN}`}</Typography>
               <BookRatings ratings={book.ratings} />
             </Stack>
           </Stack>
