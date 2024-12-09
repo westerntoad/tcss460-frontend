0bf1ef0c379067ec31b943c69a6d52b268749dd5
westerntoad <abr.engebretson@gmail.com>
2024-12-08 22:09:02 -0800

    weird home page

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,8 +1,31 @@
 'use client';
-import HomePage from "views/home-page";
+import BookIcon from 'components/BookIcon';
+import MainCard from 'components/MainCard';
+import * as React from 'react';
+import BookList from 'sections/books/BookList';
+import { IBook } from 'types/ibooks';
+import axios from 'utils/axios';
+import BookPage from 'views/book';
 
 // ==============================|| PAGE ||============================== //
 
 export default function HomeViewPage() {
-  return <HomePage />;
+  const [theData, setTheData] = React.useState<IBook[]>([]);
+  const numBooks = 9_125;
+  const numDesired = 10;
+  React.useEffect(() => {
+    let newBooks: IBook[] = [];
+    for (let i = 0; i < numDesired; i++) {
+      axios
+        .get(`/books/all/?limit=1&offset=${Math.floor(Math.random() * numBooks)}`)
+        .then((response) => {
+          newBooks.push(response.data.books[0]);
+        })
+        .catch((error) => console.error(error));
+    }
+    console.log(newBooks);
+    setTheData(newBooks);
+  }, []);
+
+  return <MainCard>{(theData.length && <BookPage book={theData[0]} />) || 'Loading...'}</MainCard>;
 }
